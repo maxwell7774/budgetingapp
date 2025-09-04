ae2edfbede48d7f3421f29bf934d6ffec6b2aef2
Adam Maxwell <maxwell7774@gmail.com>
2025-09-04 12:53:10 -0600

    Fixes some layout issues

--- webclient/src/app-layout.tsx
+++ webclient/src/app-layout.tsx
@@ -5,31 +5,13 @@ function AppLayout() {
   return (
     <>
       <Navbar />
-      <main className="w-full max-w-7xl mx-auto my-8">
-        {/* <div className="mb-8"> */}
-        {/*   <button */}
-        {/*     className="me-8 h-11 border-2 border-indigo-500 text-cyan-500 hover:bg-cyan-500/10 px-5 hover:opacity-80 hover:cursor-pointer active:opacity-60 transition-opacity rounded-full font-semibold" */}
-        {/*     type="button" */}
-        {/*   > */}
-        {/*     Add Item */}
-        {/*   </button> */}
-        {/*   <button */}
-        {/*     className="h-11 bg-indigo-50 text-cyan-600 px-5 hover:bg-cyan-100 hover:cursor-pointer active:bg-cyan-200 transition-opacity rounded-full font-semibold" */}
-        {/*     type="button" */}
-        {/*   > */}
-        {/*     Add Item */}
-        {/*   </button> */}
-        {/* </div> */}
-        {/* <div className="bg-[url(/test-pattern-2.svg)] absolute inset-0 -z-10 isolate bg-no-repeat bg-top bg-size-[100%]"> */}
-        {/* </div> */}
-        {/* <img */}
-        {/*   className=" absolute top-0 object-top object-fill" */}
-        {/*   src="test-pattern.svg" */}
-        {/* /> */}
-        <Outlet />
+      <main className="flex justify-center items-start">
+        <div className="max-w-7xl m-4 md:m-8 w-full">
+          <Outlet />
+        </div>
       </main>
-      <footer>
-        <p className="bg-slate-200 dark:bg-slate-800 p-2 px-8 w-max mx-auto rounded-full flex items-center">
+      <footer className="mx-4 md:mx-8 mb-4 md:mb-8">
+        <p className="bg-white dark:bg-slate-800 p-2 px-8 w-max mx-auto rounded-full flex items-center">
           <span className="me-1">
             Guppy Goals &bull; a product by
           </span>
--- webclient/src/components/navbar.tsx
+++ webclient/src/components/navbar.tsx
@@ -22,7 +22,7 @@ function Navbar() {
   };
 
   return (
-    <header className="sticky top-4 md:top-8 left-0 right-0 z-100 isolate">
+    <header className="sticky top-4 md:top-8 left-0 right-0 z-50 isolate mx-4 md:mx-8 mt-4 md:mt-8">
       <nav>
         <div
           data-open={open}
